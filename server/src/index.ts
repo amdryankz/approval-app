905771b3bbe79ff3cc9348d10a08421e0e239312
amdryankz <ahmad.chairiansyah@gmail.com>
2026-02-05 08:41:38 +0700

    chore: add cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,12 +3,14 @@ import { PrismaClient } from '../prisma/generated/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 import express from 'express'
 import router from './routes'
+import cors from 'cors'
 
 const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
 export const prisma = new PrismaClient({ adapter: pool })
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/api', router)
